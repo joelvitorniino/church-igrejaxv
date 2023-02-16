5c09765b1bf15e4a65d575411422501358196a3c
joelvitorniino <joelprofessor13@gmail.com>
2023-02-16 18:46:02 -0300

    feat: add new background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'black-smooth': '#333'
+      }
+    },
   },
   plugins: [],
 }
