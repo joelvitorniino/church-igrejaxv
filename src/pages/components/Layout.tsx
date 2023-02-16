92e9de0f9b36211dfe434c89ff6848c3d256eb16
joelvitorniino <joelprofessor13@gmail.com>
2023-02-16 19:09:35 -0300

    feat: add in layout footer component

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -1,12 +1,14 @@
 
 import Head from "next/head";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <>
       <Navbar />
       <main className="min-h-vh">{children}</main>
+      <Footer />
     </>
   );
 }
