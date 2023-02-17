040b7e05affc956073ee5cddf626c0f5c1ac9b81
joelvitorniino <joelprofessor13@gmail.com>
2023-02-17 16:38:48 -0300

    feat: new title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center mb-10">
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Head>
+        <title>Minha Igreja</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      </div>
+      <h1 className="text-3xl font-base flex items-center justify-center relative -top-[28rem]">Igreja XV de Novembro</h1>
     </div>
   )
 }
