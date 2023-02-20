b01939ff1a8a324d8961bfac0178ffde7c88657c
joelvitorniino <joelprofessor13@gmail.com>
2023-02-20 16:27:37 -0300

    feat: footer responsive iphone

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+    <footer className="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900 iphone:-mr-24">
       <div className="sm:flex sm:items-center sm:justify-between">
         <Link
           href="https://flowbite.com/"
