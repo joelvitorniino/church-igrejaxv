237867913bf3e41c10e0746a27de47f0d0f306e0
joelvitorniino <joelprofessor13@gmail.com>
2023-02-23 19:13:09 -0300

    feat: component email sent

---
+++ src/pages/components/EmailSent.tsx
@@ -0,0 +1,10 @@
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function EmailSent() {
+    return (
+        <div>
+            <ToastContainer />
+        </div>
+    )
+}
