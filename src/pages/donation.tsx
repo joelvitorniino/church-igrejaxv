7efbb04e8149f24103fd214dc8056bc9c4766750
joelvitorniino <joelprofessor13@gmail.com>
2023-02-22 21:16:28 -0300

    feat: page donation

---
+++ src/pages/donation.tsx
@@ -0,0 +1,25 @@
+import PixIcon from "./components/PixIcon";
+import { CiBank } from 'react-icons/ci';
+
+export default function Donation() {
+    return (
+        <div className="bg-gray-300 p-8 min-h-screen">
+        <h1 className="text-3xl font-bold">Pix da Igreja:</h1>
+        <div className="mt-4">
+          <span className="inline-block m-1">
+            <PixIcon />
+              <p className="inline ml-2 font-semibold hover:font-bold">
+                Pix da Igreja: 34.164.376/0001-53
+              </p>
+          </span>
+
+          <span className="flex m-1">
+            <CiBank size={65} className="inline ml-1" />
+              <p className="inline ml-2 font-semibold hover:font-bold mt-6">
+                Agência 2787, Conta-Corrente: 007100-5, CNPJ: 34.164.376/0001-53, Banco: Bradesco 
+              </p>
+          </span>
+        </div>
+      </div>
+    )
+}
