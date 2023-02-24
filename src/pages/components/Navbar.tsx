d42c74001c532b24d3f66f6fffe19f9fd35bc82d
joelvitorniino <joelprofessor13@gmail.com>
2023-02-24 17:48:37 -0300

    feat: add schedule page on navbar

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -55,7 +55,7 @@ export default function Navbar() {
 
                 <li className="lg:mr-[1.3em] max-sm:-mr-2 iphone:-mr-3">
                 <Link
-                    href="/"
+                    href="/schedule"
                     className="text-white no-underline p-1 transition border border-b-2 border-solid border-transparent hover:border-white"
                 >
                 Agenda 
